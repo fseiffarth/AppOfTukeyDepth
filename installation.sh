aaa0d11d047a93b7186a5c189b23b7e3f38c18ce
fseiffarth <seiffarth@cs.uni-bonn.de>
2022-06-28 11:55:13 +0200

    bash scripts added

--- installation.sh
+++ installation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Script to install the AppOfTukeyDepth code
 #prepare libs folder
-#unzip libs.zip && cd libs
+unzip libs.zip && cd libs
 #install or-tools
-#git clone https://github.com/google/or-tools && cd or-tools && make third_party && make cc && make test_cc
+git clone https://github.com/google/or-tools && cd or-tools && make third_party && make cc && make test_cc
 #install Snap-6.0
 cd .. && wget https://snap.stanford.edu/releases/Snap-6.0.zip && unzip Snap-6.0.zip
 cp fl.cpp Snap-6.0/glib-core/
